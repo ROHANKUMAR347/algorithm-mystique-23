560b64d90d4371a74f079f51227cfb96f1bca482
ROHANKUMAR347 <radhakantamendili362@gmail.com>
2024-02-26 12:56:33 +0530

    adding navbar on every page

--- script1.js
+++ script1.js
@@ -1,4 +1,4 @@
-const carousel = document.querySelector('.carousel');
+const carousel = document.querySelector(".carousel");
 const imageWidth = carousel.firstElementChild.clientWidth + 10; // Width of each image plus margin
 let currentIndex = 0;
 const lastIndex = carousel.children.length - 3; // Last index to display three images
@@ -9,16 +9,22 @@ function showNextImages() {
     currentIndex = 0; // Reset to the beginning if reached the end
     setTimeout(() => {
       const offset = -currentIndex * imageWidth * 3; // Multiply by 3 to show 3 images at once
-      carousel.style.transition = 'none'; // Disable transition to prevent animation when resetting position
+      carousel.style.transition = "none"; // Disable transition to prevent animation when resetting position
       carousel.style.transform = `translateX(${offset}px)`;
       setTimeout(() => {
-        carousel.style.transition = ''; // Re-enable transition
+        carousel.style.transition = ""; // Re-enable transition
       }, 50); // Restore transition after a brief delay
     }, 6000); // Wait for 5 seconds before resetting
   } else {
-    const offset = -currentIndex * imageWidth / 2; // Multiply by 3 to show 3 images at once
+    const offset = (-currentIndex * imageWidth) / 2; // Multiply by 3 to show 3 images at once
     carousel.style.transform = `translateX(${offset}px)`;
   }
 }
 
-setInterval(showNextImages, 6000); 
+setInterval(showNextImages, 6000);
+
+const loginbtn = document.getElementById("login");
+loginbtn.style.cursor = "pointer";
+loginbtn.addEventListener("click", () => {
+  window.location.href = "../loginPage/login.html";
+});
